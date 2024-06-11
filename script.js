16de008eb883b87f596d6a607f32fcf9bacd7baf
Irving Queval <irvingqueval@gmail.com>
2024-06-11 17:16:07 +0200

    scroll-animation added

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const navLinks = document.querySelector(".nav_links")
 menuHamburger.addEventListener('click',()=>{
     navLinks.classList.toggle('mobile_menu')
 })
+
+function scrollIntoViewWithOffset (selector, offset) {
+    window.scrollTo({
+      behavior: 'smooth',
+      top:
+        document.querySelector(selector).getBoundingClientRect().top -
+        document.body.getBoundingClientRect().top -
+        offset,
+    })
+    navLinks.classList.toggle('mobile_menu')
+  }
