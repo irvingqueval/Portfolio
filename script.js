b91a25b102017d6da789813f04c5ba1d1b791633
Irving Queval <irvingqueval@gmail.com>
2024-06-14 10:38:23 +0200

    update all file

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-const menuHamburger = document.querySelector(".menu_hamburger")
-const navLinks = document.querySelector(".nav_links")
+const menuHamburger = document.querySelector(".menu_hamburger"); 
+const navLinks = document.querySelector(".nav_links");
 
-menuHamburger.addEventListener('click',()=>{
-    navLinks.classList.toggle('mobile_menu')
-})
+menuHamburger.addEventListener('click',()=>{;
+    navLinks.classList.toggle('mobile_menu');
+});
 
-function scrollIntoViewWithOffset (selector, offset) {
+function scrollIntoViewWithOffset(selector, offset) {
     window.scrollTo({
       behavior: 'smooth',
       top:
         document.querySelector(selector).getBoundingClientRect().top -
         document.body.getBoundingClientRect().top -
         offset,
-    })
+    });
 
-    if(selector !== '#A-propos'){
-        navLinks.classList.toggle('mobile_menu')
+    if (selector !== '#A-propos') {
+        navLinks.classList.toggle('mobile_menu');
     }
   }
